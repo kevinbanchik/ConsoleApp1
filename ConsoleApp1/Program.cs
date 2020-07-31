1122d4470050e75da71fc29f63fb5016945e8462
ARIEL DAVID JAKUBOWICZ <94JA40126577@campus.economicas.uba.ar>
2020-07-31 17:04:49 -0300

    Seleccion de carreras

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,12 +11,14 @@ namespace ConsoleApp1
     {
         static private List<String[]> listaMaestroAlumnos = new List<String[]>();
         static private List<String[]> listaMaestroMateriasPorAlumno = new List<String[]>();
+        static private List<String[]> listaMaestroCarreras = new List<String[]>();
         static private int indiceAlumnoLogueado;
         static private string registroIngresado;
+        static private string carreraSeleccionada;
         static void Main(string[] args)
         {
             // agregarEstudiante(Console.ReadLine(), Console.ReadLine(), Console.ReadLine(), "maestroalumnos.csv");
-            convertirArchivosMaestros("maestroalumnos.csv", "maestromaterias.csv");
+            convertirArchivosMaestros("maestroalumnos.csv", "maestromaterias.csv", "maestrocarreras.csv");
             Console.WriteLine("Bienvenido/a al Sistema de Solicitud de Inscripción");
             
             while(!existeAlumno(listaMaestroAlumnos))
@@ -25,15 +27,28 @@ namespace ConsoleApp1
             }
                 
             Console.WriteLine("Bienvenido/a " + listaMaestroAlumnos[indiceAlumnoLogueado][0] + ' ' + listaMaestroAlumnos[indiceAlumnoLogueado][1] + Environment.NewLine);
+            carreraSeleccionada = verYElegirCarrera();
             verMateriasParaAlumno();
             Console.WriteLine("Tocá enter para cerrar el programa");
             Console.ReadLine();
 
         }
 
+        public static string verYElegirCarrera()
+        {
+            // IMPRIME CARRERAS EN PANTALLA Y DEVUELVE NUMERO DE CARRERA SELECCIONADA
+            Console.WriteLine("Ingresá el NÚMERO de la carrera de las materias a las cuales te quieras anotar" + Environment.NewLine);
+            foreach (var carrera in listaMaestroCarreras)
+            {               
+                Console.WriteLine(carrera[1] + ", Numero: " + carrera[0]);
+            }
+            Console.WriteLine(Environment.NewLine);
+            return Console.ReadLine();
+        }
+
         public static bool existeAlumno(List<String[]> maestroAlumnos)
         {
-            Console.WriteLine("Ingrese su número de registro");
+            Console.WriteLine("Ingresá tu número de registro");
             registroIngresado = Console.ReadLine();
             bool existe = false;
             int i = 0;
@@ -60,13 +75,13 @@ namespace ConsoleApp1
         public static void verMateriasParaAlumno()
         {
             Console.WriteLine("Estas son las materias a las cuales te podés inscribir:");
-            for (int i = 0; i <= listaMaestroMateriasPorAlumno.Count - 1; i++)
+
+            foreach (var materia in listaMaestroMateriasPorAlumno)
             {
-                if (listaMaestroMateriasPorAlumno[i][0] == registroIngresado)
+                if (materia[0] == registroIngresado && materia[1] == carreraSeleccionada)
                 {
-                    Console.WriteLine(listaMaestroMateriasPorAlumno[i][2] + ", Curso: " + listaMaestroMateriasPorAlumno[i][3]);
+                    Console.WriteLine(materia[2] + ", Curso: " + materia[3]);
                 }
-                
             }
             Console.WriteLine(Environment.NewLine);
         }
@@ -87,7 +102,7 @@ namespace ConsoleApp1
             }
         }
 
-        public static void convertirArchivosMaestros(string filepathAlumnos, string filepathMaterias) 
+        public static void convertirArchivosMaestros(string filepathAlumnos, string filepathMaterias, string filepathCarreras) 
         // CONVIERTE ARCHIVOS MAESTROS EN LISTAS DE ARRAYS DE STRING
         {
             using (StreamReader sr = new StreamReader(filepathAlumnos))
@@ -105,6 +120,15 @@ namespace ConsoleApp1
                 while ((linea = sr2.ReadLine()) != null)
                 {
                     listaMaestroMateriasPorAlumno.Add(linea.Split(';'));
+                }
+            }
+
+            using (StreamReader sr3 = new StreamReader(filepathCarreras))
+            {
+                string linea;
+                while ((linea = sr3.ReadLine()) != null)
+                {
+                    listaMaestroCarreras.Add(linea.Split(';'));
                 }
             }
         }
