37c0081c16fc2d0fd59b473c3210734fc64008e6
ARIEL DAVID JAKUBOWICZ <94JA40126577@campus.economicas.uba.ar>
2020-08-01 20:39:18 -0300

    ultimos cambios

--- ConsoleApp1/Materia.cs
+++ ConsoleApp1/Materia.cs
@@ -11,13 +11,13 @@ namespace ConsoleApp1
         public static List<string> indicesSeleccionados = new List<string>();
         public static void verMateriasParaAlumno()
         {
-            Console.WriteLine("Estas son las materias a las cuales te podés inscribir:");
+            Console.WriteLine(Environment.NewLine + "Estas son las materias a las cuales te podés inscribir:");
 
             foreach (var materia in Helper.listaMaestroMateriasPorAlumno)
             {
                 if (materia[1] == Alumno.registroIngresado && materia[2] == Carrera.carreraSeleccionada)
                 {
-                    Console.WriteLine(materia[3] + ", Curso: " + materia[4] + ", Índice: " + materia[0]);
+                    Console.WriteLine("- " + materia[3] + ", Curso: " + materia[4] + ", Identificador único: " + materia[0]);
                 }
             }
             Console.WriteLine(Environment.NewLine);
@@ -28,7 +28,7 @@ namespace ConsoleApp1
             string indicesIngresados;
             verMateriasParaAlumno();
 
-            Console.WriteLine("Ingresá los índices de los cursos elegidos separados por una coma (Ejemplo: 0,2,3). Recordá que podés elegir solo 1 curso por materia, y hasta 3 cursos en total." + Environment.NewLine);
+            Console.WriteLine("Ingresá los IDENTIFICADORES de los cursos elegidos separados por una coma (Ejemplo: 0,2,3). Recordá que podés elegir solo 1 curso por materia, y hasta 3 cursos en total." + Environment.NewLine);
             indicesIngresados = Console.ReadLine();
             if (indicesIngresados.Contains(','))
             {
@@ -43,6 +43,26 @@ namespace ConsoleApp1
         public static void generarSolicitudInscripcion()
         {
             List<string[]> listaMateriasElegidas = Helper.listaMaestroMateriasPorAlumno.FindAll(materia => indicesSeleccionados.Contains(materia[0]));
+
+            Console.WriteLine(Environment.NewLine + "SOLICITUD DE INSCRIPCIÓN" + Environment.NewLine);
+
+            foreach(var materia in listaMateriasElegidas)
+            {
+                try
+                {
+                    using (System.IO.StreamWriter file = new System.IO.StreamWriter("solicitud.csv", true))
+                    {
+                        file.WriteLine(materia[0] + ';' + materia[1] + ';' + materia[2] + ';' + materia[3] + ';' + materia[4]);
+                        Console.WriteLine("Materia: " + materia[3] + ", Curso: " + materia[4] + ", Identificador único: " + materia[0]);
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    throw new ApplicationException("error", ex);
+                }
+            }
+
         }
     }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,22 +12,22 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Helper.convertirArchivosMaestros("maestroalumnos.csv", "maestromaterias.csv", "maestrocarreras.csv");
-            Console.WriteLine("Bienvenido/a al Sistema de Solicitud de Inscripción");
+            Console.WriteLine("Bienvenido/a al Sistema de Solicitud de Inscripción" + Environment.NewLine);
             
             while(!Alumno.existeAlumno(Helper.listaMaestroAlumnos))
             {
                 Console.WriteLine("No existe ningún alumno con ese número de registro. Ingreselo nuevamente.");
             }
                 
-            Console.WriteLine("Bienvenido/a " + Helper.listaMaestroAlumnos[Alumno.indiceAlumnoLogueado][0] + ' ' + Helper.listaMaestroAlumnos[Alumno.indiceAlumnoLogueado][1] + Environment.NewLine);
+            Console.WriteLine(Environment.NewLine + "Bienvenido/a " + Helper.listaMaestroAlumnos[Alumno.indiceAlumnoLogueado][0] + ' ' + Helper.listaMaestroAlumnos[Alumno.indiceAlumnoLogueado][1] + Environment.NewLine);
             while (Materia.indicesSeleccionados.Count < 3)
             {
-                Console.WriteLine("Te quedan " + (3 - Materia.indicesSeleccionados.Count).ToString() + " materias para elegir");
+                Console.WriteLine(Environment.NewLine + "Te quedan " + (3 - Materia.indicesSeleccionados.Count).ToString() + " materia/s para elegir" + Environment.NewLine);
                 Carrera.verYElegirCarrera();
                 Materia.elegirMaterias();
             }
             Materia.generarSolicitudInscripcion();
-            Console.WriteLine("Tocá enter para cerrar el programa");
+            Console.WriteLine(Environment.NewLine +  "Tocá enter para cerrar el programa");
             Console.ReadLine();
 
         }
